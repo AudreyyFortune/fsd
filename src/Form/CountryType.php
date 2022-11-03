ff3d5687ba4b224faaea0ec68a12d3fc0ec4eedf
AudreyyFortune <audefortune@free.fr>
2022-11-03 14:20:01 +0100

    fix

--- src/Form/CountryType.php
+++ src/Form/CountryType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CountryType extends AbstractType
@@ -17,6 +18,10 @@ class CountryType extends AbstractType
         $builder
             ->add('isocode', TextType::class, [
 				'constraints' => [
+					new Length([
+						'max' => 2,
+						'maxMessage' => 'admin.country.form.isocode.length',
+					]),
 					new NotBlank([
 						'message' => 'admin.country.form.isocode.required',
 					]),
